4912ca1267877fb13b15a6256fd97a2c545e82fa
Alex Puschinsky <alexpoo@gmail.com>
2016-01-09 23:48:41 +0200

    Added Observer

---
+++ src/observer.js
@@ -0,0 +1,34 @@
+import {observe, unobserve} from './observe';
+import SubscriptionManager from './subscription_manager';
+
+export default class Observer{
+  observe(target, attributes, callback){
+    this._ensureInit();
+    this.subscriptionManager.subscribeTo(target, attributes, callback);
+  }
+
+  stopObservingOn(...args){
+    this._ensureInit();
+    this.subscriptionManager.unsubscribeFrom(...args);
+  }
+
+  stopObserving(){
+    this._ensureInit();
+    this.subscriptionManager.unsubscribeFromAll();
+  }
+
+  isObservingOn(target, attributes, callback){
+    this._ensureInit();
+    return this.subscriptionManager.isSubscribedTo(target, attributes, callback);
+  }
+
+  _ensureInit(){
+    if(!this.subscriptionManager){
+      this.subscriptionManager = new SubscriptionManager({
+        subscribeFn: observe,
+        unsubscribeFn: unobserve
+      });
+    }
+  }
+}
+
---
+++ tests/observer_test.js
@@ -0,0 +1,171 @@
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import Observer from '../src/observer'
+
+let expect = chai.expect;
+chai.use(sinonChai);
+
+describe('Observer', function () {
+  let target, target2, spy, spy2, observer;
+
+  beforeEach(function () {
+    target = {};
+    target2 = {};
+    spy = sinon.spy();
+    spy2 = sinon.spy();
+    observer = new Observer();
+  });
+
+  describe('observe', function () {
+    it('subscribes to a change of a given attribute in a given object', function () {
+      observer.observe(target, 'attr', spy);
+      target.attr = 'value';
+
+      expect(spy).to.have.been.called;
+    });
+
+    it('can subscribes to several attributes of a single target', function (){
+      observer.observe(target, 'attr', spy);
+      observer.observe(target, 'attr2', spy2);
+      target.attr = 'value';
+      target.attr2 = 'value';
+
+      expect(spy).to.have.been.called;
+      expect(spy2).to.have.been.called;
+    });
+
+    it('can subscribes to different targets', function (){
+      observer.observe(target, 'attr', spy);
+      observer.observe(target2, 'attr', spy2);
+      target.attr = 'value';
+      target2.attr = 'value';
+
+      expect(spy).to.have.been.called;
+      expect(spy2).to.have.been.called;
+    });
+  });
+
+  describe('isObservingOn', function () {
+    it('returns true if observing a given target', function () {
+      observer.observe(target, 'attr', spy);
+      expect(observer.isObservingOn(target)).to.equal(true);
+    });
+
+    it('returns false if not observing given target', function () {
+      observer.observe(target, 'attr', spy);
+      expect(observer.isObservingOn(target2)).to.equal(false);
+    });
+
+    it('returns true if observing given attribute', function () {
+      observer.observe(target, 'attr', spy);
+      expect(observer.isObservingOn(target, 'attr')).to.equal(true);
+    });
+
+    it('returns false if not observing given attribute', function () {
+      observer.observe(target, 'attr', spy);
+      expect(observer.isObservingOn(target, 'attr2')).to.equal(false);
+    });
+
+    it('returns true if observing given attribute with a given callback', function () {
+      observer.observe(target, 'attr', spy);
+      expect(observer.isObservingOn(target, 'attr', spy)).to.equal(true);
+    });
+
+    it('returns false if not observing given attribute with a given callback', function () {
+      observer.observe(target, 'attr', spy);
+      expect(observer.isObservingOn(target, 'attr', spy2)).to.equal(false);
+    });
+  });
+
+  describe('stopObservingOn', function () {
+    it('removes a subscribtion from an attribute in the given target', function () {
+      observer.observe(target, 'attr', spy);
+      observer.stopObservingOn(target, 'attr', spy);
+      target.attr = 'value';
+
+      expect(spy).not.to.have.been.called;
+    });
+
+    it('does nothing if called with a non observed attribute', function () {
+      observer.observe(target, 'attr', spy);
+      let fn = function(){
+        observer.stopObservingOn(target, 'attr2', spy);
+      };
+      expect(fn).not.to.throw();
+    });
+
+    it('does not effect other subscriptions', function () {
+      observer.observe(target, 'attr', spy);
+      observer.observe(target, 'attr2', spy);
+      observer.stopObservingOn(target, 'attr2', spy);
+      target.attr = 'value';
+
+      expect(spy).to.have.been.called;
+    });
+
+    it('stops observing attribute if called without a specific callback', function () {
+      observer.observe(target, 'attr', spy);
+      observer.observe(target, 'attr', spy2);
+
+      observer.stopObservingOn(target, 'attr');
+
+      target.attr = 'value';
+
+      expect(spy).not.to.have.been.called;
+      expect(spy2).not.to.have.been.called;
+    });
+
+    it('stops observing an entire target if called without an attribute', function () {
+      observer.observe(target, 'attr', spy);
+      observer.observe(target, 'attr2', spy);
+
+      observer.stopObservingOn(target);
+
+      target.attr = 'value';
+      target.attr2 = 'value';
+
+      expect(spy).not.to.have.been.called;
+    });
+  });
+
+  describe('stopObserving', function () {
+    it('stop observing to subscribed target', function () {
+      observer.observe(target, 'attr', spy);
+      observer.stopObserving();
+
+      target.attr = 'value';
+
+      expect(spy).not.to.have.been.called;
+    });
+
+    it('stop observing to several subscribed targets and attributes', function () {
+      observer.observe(target, 'attr', spy);
+      observer.observe(target, 'attr2', spy);
+      observer.observe(target2, 'attr', spy);
+      observer.observe(target2, 'attr2', spy);
+
+      observer.stopObserving();
+
+      target.attr = 'value';
+      target.attr2 = 'value';
+      target2.attr = 'value';
+      target2.attr2 = 'value';
+
+      expect(spy).not.to.have.been.called;
+    });
+
+    it('stop observing to several callbacks subscribed to same attribute', function () {
+      observer.observe(target, 'attr', spy);
+      observer.observe(target, 'attr', spy2);
+
+      observer.stopObserving();
+
+      target.attr = 'value';
+
+      expect(spy).not.to.have.been.called;
+      expect(spy2).not.to.have.been.called;
+    });
+  });
+});
