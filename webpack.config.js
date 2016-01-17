5d9b70504923e04ca0a6dcc63961d9791f1a912f
Alex Puschinsky <alexpoo@gmail.com>
2016-01-18 00:44:11 +0200

    Add babel polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 var WebpackNotifierPlugin = require('webpack-notifier');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: ['babel-polyfill', './src/index.js'],
   output: {
-      path: "dist",
-      filename: "observerkit.js",
-      libraryTarget: "umd"
+    path: 'dist',
+    filename: 'observerkit.js',
+    libraryTarget: 'umd',
   },
-  module : {
-    loaders: [ {
-        test   : /.js$/,
-        loader : 'babel-loader',
+  module: {
+    loaders: [{
+        test: /.js$/,
+        loader: 'babel-loader',
         exclude: /node_modules/,
-        query  : {
-          presets: ['es2015']
-        }
-      }
-    ]
+        query: {
+          presets: ['es2015'],
+        },
+      },
+    ],
   },
   plugins: [
     new WebpackNotifierPlugin(),
-  ]
-}
+  ],
+};
